151a314b652d6a05eed319156fdad2111ec4feff
Subhas <goalkeepersubhas07@gmail.com>
2025-08-13 01:31:46 +0530

    finish the project page and enhance the projectCard components

--- Client/src/Components/Project/ProjectCard.jsx
+++ Client/src/Components/Project/ProjectCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Card } from "../index";
 import { Image } from "../profileImage/Image";
+import { SlideUpAnimation } from "../../utils/Animation";
 
 const ProjectCard = ({
   imageURL,
@@ -11,42 +12,49 @@ const ProjectCard = ({
   liveLink,
 }) => {
   return (
-    <Card
-      wrapperClassName="md:w-[400px]"
-      padding="pt-5 px-3 pb-3"
-      background="bg-white/25"
-    >
-      <Image
-        imageURL={imageURL}
-        wrapperClass="flex justify-center"
-        className="h-60 md:w-96 rounded-md"
-      />
-      <div className="min-h-fit mt-5 w-full">
-        <p className="mt-5 h-32 max-h-60">
-          <strong>{title}</strong> {description}
-        </p>
-        <div className="mt-5 flex justify-end items-center">
-          <ul className="flex justify-between gap-6">
-            <li>
-              <Link to={githubLink} target="_blank">
-                <Image
-                  imageURL={"./Skill/github2.png"}
-                  className="h-10 w-10 text-black"
-                />
-              </Link>
-            </li>
-            <li>
-              <Link to={liveLink} target="_blank">
-                <Image
-                  imageURL={"./Projects/Website.webp"}
-                  className="h-10 w-10"
-                />
-              </Link>
-            </li>
-          </ul>
+    <SlideUpAnimation>
+      <Card
+        wrapperClassName="md:w-[400px]"
+        padding="pt-5 px-3 pb-3"
+        background="bg-white/25"
+      >
+        {/* Project images goes here */}
+        <Image
+          imageURL={imageURL}
+          wrapperClass="flex justify-center"
+          className="h-60 md:w-96 rounded-md"
+        />
+
+        {/* Project title and descriptions goes here */}
+        <div className="min-h-fit my-5 w-full">
+          <p className="mt-5 h-32 max-h-60">
+            <strong>{title}</strong> {description}
+          </p>
+
+          {/* Project links and github links goes here */}
+          <div className="mt-14 md:mt-5 flex justify-end items-center">
+            <ul className="flex justify-between gap-6">
+              <li>
+                <Link to={githubLink} target="_blank">
+                  <Image
+                    imageURL={"./Skill/github2.png"}
+                    className="h-10 w-10 text-black"
+                  />
+                </Link>
+              </li>
+              <li>
+                <Link to={liveLink} target="_blank">
+                  <Image
+                    imageURL={"./Projects/Website.webp"}
+                    className="h-10 w-10"
+                  />
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </SlideUpAnimation>
   );
 };
 
--- Client/src/Pages/Projects/Project.jsx
+++ Client/src/Pages/Projects/Project.jsx
@@ -1,11 +1,91 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, ProjectCard } from "../../Components/index";
+import { SlideInViewAnimation, SlideUpAnimation } from "../../utils/Animation";
 
 const Project = () => {
-  return (
-    <div>
-      This is my project page
-    </div>
-  )
-}
+  const [selectCategory, setSelectCategory] = useState("Frontend");
 
-export default Project
+  const projectsNav = [
+    {
+      category: "Frontend",
+    },
+    {
+      category: "Backend",
+    },
+    {
+      category: "Fullstack",
+    },
+    {
+      category: "Client Projects",
+    },
+  ];
+
+  const projectData = [
+    {
+      category: "Frontend",
+      imageURL: "./Projects/EV.jpeg",
+      title: "Electric Vehicle Dashboard",
+      description:
+        "is a React-based web application that allows users to explore detailed information about electric vehicles. Users can view specifications, track vehicle data, and explore manufacturing history based on selected years for deeper insights.",
+      githubLink: "https://github.com/Subhas6033/Electric-Vehicle-Dashboard",
+      liveLink: "https://ev-eosin.vercel.app/",
+    },
+    {
+      category: "Fullstack",
+      imageURL: "./Projects/employee.png",
+      title: "Employee Management System",
+      description:
+        "is a role-based web application that streamlines workplace operations with task assignment, real-time chat, and instant notifications. Designed to enhance collaboration between employees and co-workers while ensuring secure access control.",
+      githubLink: "https://github.com/Subhas6033/Employee-Management-System",
+      liveLink: "https://emsbysubhas.vercel.app/",
+    },
+    {
+      category: "Frontend",
+      imageURL: "./Projects/PanduAI.png",
+      title: "PANDU the AI",
+      description:
+        "is an AI-powered personal assistant that can play songs, search the internet, and launch applications using your custom voice or text commands. I have to fix the issue or bug that is occuring here is about the height of the card.",
+      githubLink: "https://github.com/Subhas6033/PANDU",
+      liveLink: "https://pandutheai.netlify.app/",
+    },
+  ];
+
+  const filteredProjects = projectData.filter(
+    (project) => project.category === selectCategory
+  );
+
+  return (
+    <SlideUpAnimation className={`min-h-screen `}>
+      <div className="pt-10 md:pt-20">
+        <ul className="flex justify-evenly items-center flex-wrap gap-3 md:gap-0">
+          {projectsNav.map((nav) => (
+            <li key={nav.category}>
+              <Button
+                children={nav.category}
+                onClick={() => setSelectCategory(nav.category)}
+                className="font-gothic text-slate-200 px-3 py-2 rounded-md border transition-all duration-300 hover:bg-gray-900 hover:cursor-pointer"
+              />
+            </li>
+          ))}
+        </ul>
+
+        {/* Project cards goes here  */}
+        <div className="py-10 px-5 flex justify-evenly items-center flex-wrap gap-10">
+          {filteredProjects.length > 0 ? (
+            filteredProjects.map((projects, index) => (
+              <SlideInViewAnimation key={index}>
+                <ProjectCard {...projects} />
+              </SlideInViewAnimation>
+            ))
+          ) : (
+            <p className="text-2xl text-white text-center pt-10">
+              Currently no projects is available here
+            </p>
+          )}
+        </div>
+      </div>
+    </SlideUpAnimation>
+  );
+};
+
+export default Project;
